cf20be93a33bd3ec795749c9b7d3b3d3f77c866f
drinzunza <86790118+drinzunza@users.noreply.github.com>
2021-08-02 15:47:56 -0700

    Add files via upload

---
+++ organika/src/App.js
@@ -0,0 +1,24 @@
+import "./App.css";
+import NavBar from "./components/navBar";
+import Footer from "./components/footer";
+import Catalog from "./components/catalog";
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar></NavBar>
+
+      <Catalog></Catalog>
+
+      <Footer></Footer>
+    </div>
+  );
+}
+
+export default App;
+
+/*
+Invesitation HW:
+javascript this
+arrow function
+*/
---
+++ organika/src/components/catalog.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Catalog extends Component {
+  state = {};
+  render() {
+    return (
+      <div className="catalog">
+        <h3>Our amazing catalog</h3>
+      </div>
+    );
+  }
+}
+
+export default Catalog;
